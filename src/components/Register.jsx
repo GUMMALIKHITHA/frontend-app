374389091230b44e96cde3c4fd26234fc60f303f
GUMMALIKHITHA <gummalikhitha679@gmail.com>
2026-03-12 12:24:32 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 function Register() {
+  const [user,setUser] = useState({});
+  const API_URL = import.meta.env.VITE_API_URL;
+  const Navigate = useNavigate
+  const handleSubmit = async () => {}
   return (
     <div>
       <h2>Registration Page</h2>
-      <p><input type="text" placeholder="Name" /></p>
+      <p><input type="text"onChange={(e)=>setUser({...user,name:e.target.value})} placeholder="Name" /></p>
       <p><input type="text" placeholder="Email" /></p>
       <p><input type="password" placeholder="Password" /></p>
       <p><button>Submit</button></p>
